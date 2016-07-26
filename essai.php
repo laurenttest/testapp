a018fcc804fb7db7945981f4ef7800a690b45503
laurent <you@example.com>
2016-07-26 11:25:37 +0200

    test modification par reference

--- essai.php
+++ essai.php
@@ -19,3 +19,42 @@ foreach ($generator as $line)
 {
   echo "---> ".$line."<br>";// Effectuer une opération sur $line
 }
+
+
+class SomeClass
+{
+  protected $attr;
+
+  public function __construct()
+  {
+    $this->attr = ['Un', 'Deux', 'Trois', 'Quatre'];
+  }
+
+  // Le & avant le nom du générateur indique que les valeurs retournées sont des références
+  public function &generator()
+  {
+    // On cherche ici à obtenir les références des valeurs du tableau pour les retourner
+    foreach ($this->attr as &$val)
+    {
+      yield $val;
+    }
+  }
+
+  public function attr()
+  {
+    return $this->attr;
+  }
+}
+
+$obj = new SomeClass;
+
+// On parcourt notre générateur en récupérant les entrées par référence
+foreach ($obj->generator() as &$val)
+{
+  // On effectue une opération quelconque sur notre valeur
+  $val = strrev($val);
+}
+
+echo '<pre>';
+var_dump($obj->attr());
+echo '</pre>';
